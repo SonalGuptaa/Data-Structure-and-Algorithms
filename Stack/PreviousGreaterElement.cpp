abab6e13e6507f577ac7a65ac73b80f7598dd653
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2023-07-16 13:34:50 +0530

    new_codes

---
+++ Stack/PreviousGreaterElement.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int prevgreater(int arr[],int n)
+{
+    stack<int> s;
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+
+}
+int main()
+{
+    int arr[]={15,10,18,12,4,6,2,8};
+    int n=sizeof(arr);
+
+}
